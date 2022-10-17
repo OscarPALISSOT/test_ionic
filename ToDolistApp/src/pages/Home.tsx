f35a53a73eda3c674ca8d90c33ae281633e70179
Oscar PALISSOT <oscar.p25000@gmail.com>
2022-10-17 23:44:07 +0200

    test

--- ToDolistApp/src/pages/Home.tsx
+++ ToDolistApp/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import HomeContainer from '../components/HomeContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,16 +7,16 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+            <IonTitle><strong>ToDoApp</strong></IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">ToDo</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <HomeContainer />
       </IonContent>
     </IonPage>
   );
