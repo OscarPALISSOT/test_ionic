88ab14ef121239d278af3e2ee406514a52dcd4c4
Oscar PALISSOT <oscar.p25000@gmail.com>
2022-10-19 23:55:00 +0200

    push new page histiry

--- ToDolistApp/src/pages/Home/index.tsx
+++ ToDolistApp/src/pages/Home/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import {IonContent, IonButton, IonPage} from '@ionic/react';
 import './Home.css';
 import Header from '../../components/header'
+import { RouteComponentProps } from "react-router";
 
 
-const Home: React.FC = () => {
+const Home: React.FC<RouteComponentProps> = (props) => {
     return (
         <IonPage>
             <Header/>
@@ -12,7 +13,7 @@ const Home: React.FC = () => {
                 <div className="container">
                     <strong>Hello world</strong>
                     <p>Make amazing list!</p>
-                    <IonButton href="/list" class="ion-margin" color="warning">Let's go!</IonButton>
+                    <IonButton onClick={() => props.history.push('/list')} class="ion-margin" color="warning">Let's go!</IonButton>
                 </div>
             </IonContent>
         </IonPage>
